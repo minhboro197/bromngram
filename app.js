8464724eba06e25f8009a5e4ce58c5f2ef1be71c
minhboro197 <87398863+minhboro197@users.noreply.github.com>
2022-09-14 15:15:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser')
 const musicstoreRouter = require('./Routers/musicstore')
 const authenticateRouter = require('./Routers/user')
 const accessRouter = require('./Routers/access')
-
+require('dotenv').config()
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
